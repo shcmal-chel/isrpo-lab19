bc6f91910ada764ce640f42641138c36a32ccd3f
shcmal-chel <shcmalivan@gmail.com>
2026-03-25 09:21:56 +0300

    Step 8: objects in JavaScript

--- main.js
+++ main.js
@@ -1,108 +1,108 @@
-for (let i = 0; i < 5; i++){
-    console.log(i);
+for (let i = 0; i < 5; i++) {
+  console.log(i);
 }
 
-for (let i = 0; i<=10; i++) {
-    if (i % 2){
-    } else {
-        console.log(i)
-    }
+for (let i = 0; i <= 10; i++) {
+  if (i % 2) {
+  } else {
+    console.log(i);
+  }
 }
 
 let count = 0;
 while (count < 3) {
-    console.log("Count:",count);
-    count++;
+  console.log("Count:", count);
+  count++;
 }
 
 let number = 5;
-while (number != -1){
-    console.log(number)
-    number--;
+while (number != -1) {
+  console.log(number);
+  number--;
 }
 
 let doValue = 0;
 do {
-    console.log("Value:", doValue);
-    doValue++;
+  console.log("Value:", doValue);
+  doValue++;
 } while (doValue < 3);
 
-for (let i = 0; i < 10; i++){
-    if (i === 5){
-        break;
-    }
-    console.log(i);
+for (let i = 0; i < 10; i++) {
+  if (i === 5) {
+    break;
+  }
+  console.log(i);
 }
 
-for ( let i = 0; i < 10; i++){
-    if (i === 5){
-        continue;
-    }
-    if (i === 9){
-        break;
-    }
-    console.log(i)
+for (let i = 0; i < 10; i++) {
+  if (i === 5) {
+    continue;
+  }
+  if (i === 9) {
+    break;
+  }
+  console.log(i);
 }
 
-for (let i = 1; i <= 3; i++){
-    for (let j = 1; j <= 3; j++){
-        console.log(`i = ${i}, j = ${j}`);
-    }
+for (let i = 1; i <= 3; i++) {
+  for (let j = 1; j <= 3; j++) {
+    console.log(`i = ${i}, j = ${j}`);
+  }
 }
 
-for (let i = 1; i <= 4; i++){
-    let symbol = ""
-    for (let j = 1; j <= i; j++){
-        symbol+="*";
-    }
-    console.log(symbol)
+for (let i = 1; i <= 4; i++) {
+  let symbol = "";
+  for (let j = 1; j <= i; j++) {
+    symbol += "*";
+  }
+  console.log(symbol);
 }
 
-function sum(a,b){
-    return a + b;
+function sum(a, b) {
+  return a + b;
 }
-console.log(sum(3,5));
+console.log(sum(3, 5));
 
-function multiply(a, b){
-    return a * b;
+function multiply(a, b) {
+  return a * b;
 }
-console.log(multiply(2,10));
+console.log(multiply(2, 10));
 
 function sayHello(name) {
-    console.log(`Hello, ${name}`);
+  console.log(`Hello, ${name}`);
 }
 sayHello("Тимофей");
 
-function printInfo(name, age){
-    console.log(`Hello, ${name}.\nYour age: ${age}.`)
+function printInfo(name, age) {
+  console.log(`Hello, ${name}.\nYour age: ${age}.`);
 }
-printInfo("Олег",25)
+printInfo("Олег", 25);
 
 function greet(name = "Гость") {
-    console.log("Привет, " + name);
+  console.log("Привет, " + name);
 }
 greet();
 greet("Анастасия");
 
-function calculateDiscount(count, procent){
-    if (procent == 0){
-        procent = 10
-    }
-    console.log(`Итоговая цена: ${(count * procent) / 100}.`);
+function calculateDiscount(count, procent) {
+  if (procent == 0) {
+    procent = 10;
+  }
+  console.log(`Итоговая цена: ${(count * procent) / 100}.`);
 }
-calculateDiscount(100,0);
+calculateDiscount(100, 0);
 
-const add = function (a,b) {
-    return a + b;
+const add = function (a, b) {
+  return a + b;
 };
 console.log(add(2, 3));
 
 function makeCounter() {
-    let count = 0;
-    return function (){
-        count++;
-        return count;
-    };
+  let count = 0;
+  return function () {
+    count++;
+    return count;
+  };
 }
 
 const counter = makeCounter();
@@ -110,16 +110,16 @@ console.log(counter());
 console.log(counter());
 console.log(counter());
 
-function sumFunc(a, b){
-    return a + b;
+function sumFunc(a, b) {
+  return a + b;
 }
 const sumFunc2 = (a, b) => a + b;
 const sayGreeting = () => console.log("Hello");
-const square = x => x * x;
-const calculate = (a,b) => {
-    let result = a + b;
-    return result * 2;
-}
+const square = (x) => x * x;
+const calculate = (a, b) => {
+  let result = a + b;
+  return result * 2;
+};
 
 let numbersArr = [1, 2, 3, 4, 5];
 console.log(numbersArr);
@@ -127,10 +127,10 @@ console.log(numbersArr[0]);
 console.log(numbersArr[1]);
 
 let colors = ["Red", "Green", "Blue"];
-console.log(colors[0],colors[2])
-console.log(colors)
+console.log(colors[0], colors[2]);
+console.log(colors);
 
-console.log(numbersArr.length)
+console.log(numbersArr.length);
 numbersArr.push(10);
 console.log(numbersArr);
 numbersArr.pop();
@@ -138,11 +138,11 @@ console.log(numbersArr);
 
 let numbers2 = [10, 20, 30];
 for (let i = 0; i < numbers2.length; i++) {
-    console.log(numbers2[i]);
+  console.log(numbers2[i]);
 }
 
-for (let value of numbers2){
-    console.log(value);
+for (let value of numbers2) {
+  console.log(value);
 }
 
 let mixedArray = [1, "text", true, 3.14];
@@ -155,6 +155,74 @@ console.log(numbersArr.indexOf(2));
 //console.log(fruits.includes("манго"));
 
 let cityArr = ["Moscow", "Volgograd", "Vena", "Bejing", "New-York"];
-if(cityArr.includes("Volgograd") == true){
-    console.log(cityArr.indexOf("Volgograd"))
+if (cityArr.includes("Volgograd") == true) {
+  console.log(cityArr.indexOf("Volgograd"));
 }
+
+let user2 = {
+  name: "Ivan",
+  age: 20,
+  isStudent: true,
+};
+
+console.log(user2);
+
+console.log(user2.name);
+console.log(user2.age);
+console.log(user2["name"]);
+
+let book = {
+  title: "Как найти грибы в парке",
+  author: "Mister Shmurdyak",
+  year: 2026,
+};
+
+console.log(book);
+console.log(book.age);
+
+user2.age = 30;
+user2.name = "Кирилл";
+delete user2.isStudent;
+console.log(user2);
+
+let user3 = {
+  name: "Ivan",
+  age: 20,
+  isStudent: true,
+  sayHello: function () {
+    console.log(`Hello< my name is ${name}`);
+  },
+};
+
+let car = {
+  brand: "Shkonka",
+  year: 1936,
+  getInfo: function () {
+    console.log(`Car brand is - ${this.brand}, year made in - ${year}`);
+  },
+};
+
+for (let key in user2) {
+  console.log(key + ": " + user2[key]);
+}
+
+let product = {
+  name: "PIVO",
+  price: 69,
+};
+
+for (let key in product) {
+  console.log(key + ": " + product[key]);
+}
+
+let student = {
+  name: "Бедрос",
+  skills: ["HTML", "CSS", "JS"],
+  address: {
+    city: "Выхино",
+    street: "Ганкстерская",
+  },
+};
+
+console.log(student.skills[0]);
+console.log(student.address.city);
